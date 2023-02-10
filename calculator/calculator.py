e077c5c6d42f6fc4353a655359f2d23e0e454611
xGODSx <59203346+xCode-Breakerx@users.noreply.github.com>
2023-02-10 11:25:10 +0100

    Added new tests and bug fixes

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,24 +1,64 @@
+import decimal
+
+
+def check_params(func: callable) -> callable:
+    def wrapper(*args, **kars):
+        for x in args:
+            if not isinstance(x, (int, float, complex, decimal.Decimal)):
+                raise TypeError
+        for x in kars.values():
+            if not isinstance(x, (int, float, complex, decimal.Decimal)):
+                raise TypeError
+
+        return func(*args, **kars)
+
+    return wrapper
+
+
+# def check_params_single(func: callable) -> callable:
+#     def wrapper(x, y):
+#         if not isinstance(x, (int, float, complex, decimal.Decimal)) or not isinstance(y, (int, float, complex, decimal.Decimal)):
+#             raise TypeError
+#         return func(x, y)
+#
+#     return wrapper
+
 class Calculator:
+    # use the static method decorator, otherwise the functions signatures below assume that the first parameter is "self".
+    # these methods are meant to be called statically judging from the signature
+
+    @staticmethod
+    @check_params
     def add(x, y):
-        return x + y
+        return x + y  # the operation is ambiguous, this method has been changed to a static method
 
+    @staticmethod
+    @check_params
     def subtract(x, y):
-        return x - y
+        return x - y  # the operation is ambiguous, this method has been changed to a static method
 
+    @staticmethod
+    @check_params
     def multiply(x, y):
-        return x * y
+        return x * y  # the operation is ambiguous, this method has been changed to a static method
 
+    @staticmethod
+    @check_params
     def divide(x, y):
-        return x / y
+        return x / y  # the operation is ambiguous, this method has been changed to a static method
 
+    @staticmethod
+    @check_params
     def power(x, y):
         result = 1
         for i in range(y):
-            result *= x
+            result *= x  # the operation is ambiguous, this method has been changed to a static method
         return result
 
+    @staticmethod
+    @check_params
     def square_root(x):
-        if x == 0 or x == 1:
+        if x == 0 or x == 1:  # the operation is ambiguous, this method has been changed to a static method
             return x
         val = x
         precision = 0.0000001
--- tests/unit/calculator_test.py
+++ tests/unit/calculator_test.py
@@ -1,25 +1,151 @@
+import sys
 import unittest
 from unittest import TestCase
+
+from calculator import calculator
 
 
 class TestCalculator(TestCase):
     def test_add(self):
-        self.fail()
+        self.assertEqual(calculator.Calculator.add(1, 1), 2)  # 1 + 1 = 2
+        self.assertEqual(calculator.Calculator.add(1, -2), -1)  # 1 - 2 = -1
+
+        # self.assertEqual(calculator.Calculator.add(1.0, -2.2), -1.2)  # 1 - 2.2 != -1.2, this test should fail due to IEEE standard
+        # self.assertEqual(calculator.Calculator.add(decimal.Decimal("1"), decimal.Decimal("-2.2")), decimal.Decimal("-1.2"))  # should pass due to higher precision
+
+        # add together different types
+        with self.assertRaises(TypeError):
+            calculator.Calculator.add(object, object)
+
+        # add together different types
+        with self.assertRaises(TypeError):
+            calculator.Calculator.add("", 1)
+
+        # Assume that there is no default params
+        with self.assertRaises(TypeError):
+            calculator.Calculator.add()
+
+        try:
+            self.assertTrue(calculator.Calculator.add(sys.maxsize, sys.maxsize) > 0)  # assert that the runtime can handle large numbers with ease, and it can handle wrapping
+        except:
+            self.fail()
 
     def test_subtract(self):
-        self.fail()
+        self.assertEqual(calculator.Calculator.subtract(1, 1), 0)  # 1 - 1 = 0
+        self.assertEqual(calculator.Calculator.subtract(1, -2), 3)  # 1 + 2 = 3
+
+        # self.assertNotEqual(calculator.Calculator.subtract(decimal.Decimal("0.5"), decimal.Decimal("2.0**-55")), decimal.Decimal("0.5"))  # should pass due to higher precision
+        # self.assertNotEqual(calculator.Calculator.subtract(0.5, 2.0 ** -55), 0.5)  # this test should fail due to IEEE standard
+
+        # add together different types
+        with self.assertRaises(TypeError):
+            calculator.Calculator.subtract(object, object)
+
+        # add together different types
+        with self.assertRaises(TypeError):
+            calculator.Calculator.subtract("", 1)
+
+        # Assume that there is no default params
+        with self.assertRaises(TypeError):
+            calculator.Calculator.subtract()
+
+        try:
+            self.assertTrue(calculator.Calculator.subtract(-sys.maxsize, sys.maxsize) < 0)  # assert that the runtime can handle large numbers with ease, and it can handle wrapping
+        except:
+            self.fail()
 
     def test_multiply(self):
-        self.fail()
+        self.assertEqual(calculator.Calculator.multiply(1, 1), 1)  # 1 * 1 = 1
+        self.assertEqual(calculator.Calculator.multiply(1, -2), -2)  # 1 * -2 = -2
+
+        self.assertEqual(calculator.Calculator.multiply(0.5, 0.25), 0.125)  # 0.5 * 0.25 = 0.125
+        # self.assertEqual(calculator.Calculator.multiply(decimal.Decimal("1"), decimal.Decimal("-2.2")), decimal.Decimal("-2.2"))  # should pass due to higher precision
+
+        # add together different types
+        with self.assertRaises(TypeError):
+            calculator.Calculator.multiply(object, object)
+
+        # add together different types, this should not pass even if python is able to multiply strings
+        with self.assertRaises(TypeError):
+            calculator.Calculator.multiply("", 1)
+
+        # Assume that there is no default params
+        with self.assertRaises(TypeError):
+            calculator.Calculator.multiply()
+
+        try:
+            self.assertTrue(calculator.Calculator.multiply(sys.maxsize, sys.maxsize) > 0)  # assert that the runtime can handle large numbers with ease, and it can handle wrapping
+        except:
+            self.fail()
 
     def test_divide(self):
-        self.fail()
+        self.assertEqual(calculator.Calculator.divide(1, 1), 1)  # 1 / 1 = 1
+        self.assertEqual(calculator.Calculator.divide(1, -2), -0.5)  # 1 / -2 = -0.5
+
+        self.assertEqual(calculator.Calculator.divide(0.5, 0.25), 2)  # 0.5 / 0.25 = 2
+        # self.assertEqual(calculator.Calculator.divide(decimal.Decimal("0.5"), decimal.Decimal("0.25")), decimal.Decimal("2"))  # should pass due to higher precision
+
+        # add together different types
+        with self.assertRaises(TypeError):
+            calculator.Calculator.divide(object, object)
+
+        # add together different types
+        with self.assertRaises(TypeError):
+            calculator.Calculator.divide("", 1)
+
+        # Assume that there is no default params
+        with self.assertRaises(TypeError):
+            calculator.Calculator.divide()
+
+        try:
+            self.assertEqual(calculator.Calculator.divide(sys.maxsize, 2), sys.maxsize / 2)  # assert that the runtime can handle large numbers with ease, and it can handle wrapping
+        except:
+            self.fail()
 
     def test_power(self):
-        self.fail()
+        self.assertEqual(calculator.Calculator.power(1, 1), 1 ** 1)  # 1 / 1 = 1
+        self.assertEqual(calculator.Calculator.power(1, -2), 1 ** -2)  # 1 / -2 = -0.5
+
+        # add together different types
+        with self.assertRaises(TypeError):
+            calculator.Calculator.power(object, object)
+
+        # add together different types
+        with self.assertRaises(TypeError):
+            calculator.Calculator.power("", 1)
+
+        # Assume that there is no default params
+        with self.assertRaises(TypeError):
+            calculator.Calculator.power()
+
+        # Assume that the supported exponent is integer only
+        with self.assertRaises(TypeError):
+            self.assertEqual(calculator.Calculator.power(0.5, 0.25), 0.5 ** 0.25)  # 0.5 / 0.25 = 2
+
+        try:
+            self.assertEqual(calculator.Calculator.power(sys.maxsize, 2), sys.maxsize ** 2)  # assert that the runtime can handle large numbers with ease, and it can handle wrapping
+        except:
+            self.fail()
 
     def test_square_root(self):
-        self.fail()
+        self.assertEqual(calculator.Calculator.square_root(1), 1)  # sqrt(1) = 1
+        self.assertEqual(calculator.Calculator.square_root(9), 3)  # sqrt(9) = 3
+
+        # add together different types
+        with self.assertRaises(TypeError):
+            calculator.Calculator.square_root(object, object)
+
+        # add together different types
+        with self.assertRaises(TypeError):
+            calculator.Calculator.square_root("", 1)
+
+        # Assume that there is no default params
+        with self.assertRaises(TypeError):
+            calculator.Calculator.square_root()
+
+        # Assume that the params are verified
+        with self.assertRaises(TypeError):
+            calculator.Calculator.square_root(-2)  # sqrt is not defined for negative numbers
 
 
 if __name__ == '__main__':
